e002f92b87c646c6ec7a30569898cc75cc28d9b2
happyo <xiaxiongzhi@corp.netease.com>
2021-11-29 14:47:20 +0800

    add use package

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -61,8 +61,27 @@ locate PACKAGE."
 
 ;;; Fire up package.el
 
-(setq package-enable-at-startup nil)
-(package-initialize)
+;; Initialize packages
+(unless (bound-and-true-p package--initialized) ; To avoid warnings in 27
+  (setq package-enable-at-startup nil)          ; To prevent initializing twice
+  (package-initialize))
+
+;; Setup `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Should set before loading `use-package'
+(eval-and-compile
+  (setq use-package-always-ensure t)
+  (setq use-package-always-defer t)
+  (setq use-package-expand-minimally t)
+  (setq use-package-enable-imenu-support t))
+
+(eval-when-compile
+  (require 'use-package))
+;; (setq package-enable-at-startup nil)v
+;; (package-initialize)
 
 
 ;; package.el updates the saved version of package-selected-packages correctly only
@@ -115,6 +134,35 @@ advice for `require-package', to which ARGS are passed."
 ;;       (sanityinc/set-tabulated-list-column-width "Archive" longest-archive-name))))
 
 ;; (add-hook 'package-menu-mode-hook 'sanityinc/maybe-widen-package-menu-columns)
+
+;; A modern Packages Menu
+(use-package paradox
+  :init
+  (setq paradox-execute-asynchronously t
+        paradox-github-token t
+        paradox-display-star-count nil)
+
+  ;; Replace default `list-packages'
+  (defun my-paradox-enable (&rest _)
+    "Enable paradox, overriding the default package-menu."
+    (paradox-enable))
+  (advice-add #'list-packages :before #'my-paradox-enable)
+  :config
+  (when (fboundp 'page-break-lines-mode)
+    (add-hook 'paradox-after-execute-functions
+              (lambda (&rest _)
+                (let ((buf (get-buffer-create "*Paradox Report*"))
+                      (inhibit-read-only t))
+                  (with-current-buffer buf
+                    (page-break-lines-mode 1))))
+              t)))
+
+;; Auto update packages
+(use-package auto-package-update
+  :init
+  (setq auto-package-update-delete-old-versions t
+        auto-package-update-hide-results t)
+  (defalias 'upgrade-packages #'auto-package-update-now))
 
 
 (provide 'init-elpa)
