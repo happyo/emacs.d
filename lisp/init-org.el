d6c2b9f5e01c014b4e00fa138f533f70cfa14870
happyo <xiaxiongzhi@corp.netease.com>
2022-03-28 19:38:21 +0800

    modify org

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -42,14 +42,14 @@
                        ))
   (org-todo-keyword-faces
    '(
-          ("NOTES" . "#FA5055")
-          ("BOLD_P" . "#FF6B47")
-          ("HIGHLIGHT_P" . "#E2B17C")
-          ("MINI_SUM" . "#468FE0")
-          ("REMIX" . "#00CC7E")
-          ("TODO" . "#FA5055")
-          ("DONE" . "$00CC7E")
-          ))
+     ("NOTES" . "#FA5055")
+     ("BOLD_P" . "#FF6B47")
+     ("HIGHLIGHT_P" . "#E2B17C")
+     ("MINI_SUM" . "#468FE0")
+     ("REMIX" . "#00CC7E")
+     ("TODO" . "#FA5055")
+     ("DONE" . "$00CC7E")
+     ))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
@@ -69,37 +69,37 @@
   :hook ((org-mode . org-modern-mode)
          (org-agenda-finalize . org-morden-agenda))
   :config
-  (modify-all-frames-parameters
- '((right-divider-width . 40)
-   (internal-border-width . 40)))
-(dolist (face '(window-divider
-                window-divider-first-pixel
-                window-divider-last-pixel))
-  (face-spec-reset-face face)
-  (set-face-foreground face (face-attribute 'default :background)))
-(set-face-background 'fringe (face-attribute 'default :background))
+  ;; (modify-all-frames-parameters
+  ;;  '((right-divider-width . 40)
+  ;;    (internal-border-width . 40)))
+  ;; (dolist (face '(window-divider
+  ;;                 window-divider-first-pixel
+  ;;                 window-divider-last-pixel))
+  ;;   (face-spec-reset-face face)
+  ;;   (set-face-foreground face (face-attribute 'default :background)))
+  ;; (set-face-background 'fringe (face-attribute 'default :background))
 
-(setq
- ;; Edit settings
- org-auto-align-tags nil
- org-tags-column 0
- org-catch-invisible-edits 'show-and-error
- org-special-ctrl-a/e t
- org-insert-heading-respect-content t
+  (setq
+   ;; Edit settings
+   org-auto-align-tags nil
+   org-tags-column 0
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
 
- ;; Org styling, hide markup etc.
- org-hide-emphasis-markers t
- org-pretty-entities t
- org-ellipsis "…"
+   ;; Org styling, hide markup etc.
+   org-hide-emphasis-markers t
+   org-pretty-entities t
+   org-ellipsis "…"
 
- ;; Agenda styling
- org-agenda-block-separator ?─
- org-agenda-time-grid
- '((daily today require-timed)
-   (800 1000 1200 1400 1600 1800 2000)
-   " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
- org-agenda-current-time-string
- "⭠ now ─────────────────────────────────────────────────"))
+   ;; Agenda styling
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   org-agenda-current-time-string
+   "⭠ now ─────────────────────────────────────────────────"))
 
 (use-package org
   :init
