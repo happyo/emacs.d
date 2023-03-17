66906953d730ec332512652a22ec49c1155ddc2c
happyo <xiaxiongzhi@corp.netease.com>
2023-03-17 16:18:07 +0800

    @dev # Add chat

---
+++ lisp/init-chat.el
@@ -0,0 +1,10 @@
+;;; init-chat.el --- Insert description here -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/mind-wave")
+
+(require 'mind-wave)
+
+(provide 'init-chat)
+;;; init-chat.el ends here
