e945b349ccce2d9872758570b99340f1a1b2fa5f
happyo <xiaxiongzhi@corp.netease.com>
2023-04-28 16:04:23 +0800

    Modify nerd fonts

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -66,7 +66,14 @@
   :if (display-graphic-p))
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp/treemacs-nerd-icons")
-(require 'nerd-icons)
+(use-package nerd-icons
+  :custom
+  ;; The Nerd Font you want to use in GUI
+  ;; "Symbols Nerd Font Mono" is the default and is recommended
+  ;; but you can use any other Nerd Font if you want
+  (nerd-icons-font-family "Symbols Nerd Font Mono")
+  )
+
 
 (use-package rainbow-mode
   :init
