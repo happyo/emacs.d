019e5ea5f73ff01d5eeef86364d719136fc589de
happyo <xiaxiongzhi@corp.netease.com>
2024-06-09 21:30:44 +0800

    @dev # Add cursor animation

--- lisp/init-macos.el
+++ lisp/init-macos.el
@@ -2,9 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/EmacsMacOSModule")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/EmacsMacPluginModule")
 
-(require 'macos)
+(require 'mac-plugin)
+
+(mac-plugin-load-release)
+(atmosphere-enable)
+(mac-plugin-set-cursor-color "#fcc800")
+(mac-plugin-set-shadow-opacity 1)
 
 (provide 'init-macos)
 ;;; init-macos.el ends here
