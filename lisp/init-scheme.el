b7bf21b860abc66095dc3a2cf71e74e2a5564be5
happyo <xiaxiongzhi@corp.netease.com>
2024-06-25 15:29:00 +0800

    @dev # Update

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -4,15 +4,11 @@
 ;; (require-package 'scheme)
 ;; (require-package 'geiser-chez)
 ;; (require-package 'use-package)
-;; (require-package 'flycheck-guile)
 
 ;; (use-package scheme
 ;;   :Hook (scheme-mode . rainbow-delimiters-mode)
 ;;   :config (advice-add #'scheme-indent-function :override #'+scheme-scheme-indent-function-a))
 
-
-;; (use-package flycheck-guile
-;;   :after geiser)
 
 (provide 'init-scheme)
 ;;; init-scheme.el ends here
