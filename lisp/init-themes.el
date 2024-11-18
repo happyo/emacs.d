3938c44d2532f85c13291df6325641a7ddf51e14
happyo <xiaxiongzhi@corp.netease.com>
2024-11-18 19:59:18 +0800

    feat(themes): replace ef-themes with modus-themes in configuration

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,12 +4,13 @@
 
 (require 'init-elpa)
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/ef-themes")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/modus-themes")
+(require 'modus-themes)
+
 (add-to-list 'load-path "~/.emacs.d/site-lisp/chinese-color")
-(require 'ef-themes)
 (require 'chinese-color)
 
-(setq ef-summer-palette-overrides
+(setq modus-operandi-palette-overrides
       `(
 ;;; Basic values
         ;; (bg-main     ,tcc-351-ningzhi)
@@ -18,9 +19,7 @@
         ;; (bg-main     "#FFFBE9")
         ;; (bg-main     "#eff1f5")
         (bg-main     "#FFFFFF")
-        ;; (fg-main     ,tcc-015-wanse)
         (fg-main     "#445682")
-        ;; (fg-main     "#CC99FF")
         (bg-dim      ,tcc-118-tianqing)
         (fg-dim      "#777294")
         
@@ -158,20 +157,25 @@
         ;; (prompt magenta-warmer)
 
 ;;;; Code mappings
-
+         (bracket fg-main)
         (builtin magenta)
         (comment ,tcc-317-hehui)
-        ;; (constant red-cooler)
-        ;; (fnname blue-cooler)
+      ;; (constant blue-cooler)
+      ;; (delimiter fg-main)
+      ;; (docmarkup magenta-faint)
+      ;; (docstring cyan-faint)
         (fnname ,tcc-158-baiqing)
         (keyword magenta-cooler)
-        ;; (preprocessor green-warmer)
-        ;; (docstring cyan-faint)
+        (number blue-cooler)
+        (operator blue-cooler)
+        (preprocessor red-cooler)
+        (punctuation fg-main)
+        (rx-backslash magenta)
+        (rx-construct green-cooler)
         (string red-warmer)
         (type cyan-warmer)
         (variable red-warmer)
-        ;; (rx-escape cyan-cooler) ; compare with `string'
-        ;; (rx-construct red-cooler)
+
 
 ;;;; Accent mappings
 
@@ -232,75 +236,25 @@
         (rainbow-6 ,tcc-013-meihong)
         ))
 
-(setq ef-themes-to-toggle '(ef-spring ef-autumn))
+(load-theme 'modus-operandi :no-confirm)
 
-;; (setq ef-themes-headings ; read the manual's entry or the doc string
-;;       '((0 . (variable-pitch light 2.3))
-;;         (1 . (variable-pitch light 1.6))
-;;         (2 . (variable-pitch regular 1.6))
-;;         (3 . (variable-pitch regular 1.4))
-;;         (4 . (variable-pitch regular 1.3))
-;;         (5 . (variable-pitch 1.0)) ; absence of weight means `bold'
-;;         (6 . (variable-pitch 1.0))
-;;         (7 . (variable-pitch 1.0))
-;;         (t . (variable-pitch 1.0))))
-
-(setq ef-themes-mixed-fonts t
-      ef-themes-variable-pitch-ui t)
-
-;; Read the doc string or manual for this one.  The symbols can be
-;; combined in any order.
-(setq ef-themes-region '(intense no-extend neutral))
-
-;; Disable all other themes to avoid awkward blending:
-(mapc #'disable-theme custom-enabled-themes)
-
-;; ;; Using the hook lets our changes persist when we use the commands
-;; ;; `ef-themes-toggle', `ef-themes-select', and `ef-themes-load-random'.
-;; (add-hook 'ef-themes-post-load-hook #'my-ef-themes-custom-faces)
-
-;; Load the theme and run `ef-themes-post-load-hook'
-(ef-themes-select 'ef-summer) ; Instead of (load-theme 'ef-summer :no-confirm)
-
-;; Load the theme of choice:
-;; (ef-themes-select 'ef-spring)
-
-                                        ; original value is blue-warmer
-
-;; The themes we provide:
-;;      ;; (bg-main     ,tcc-351-ningzhi)
-;; Light: `ef-day', `ef-light', `ef-spring', `ef-summer'.
-;; Dark:  `ef-autumn', `ef-dark', `ef-night', `ef-winter'.
-;;
-;; Also those which are optimized for deuteranopia (red-green color
-;; deficiency): `ef-deuteranopia-dark', `ef-deuteranopia-light'.
-
-;; We also provide these commands, but do not assign them to any key:
-;;
-;; - `ef-themes-toggle'
-;; - `ef-themes-select'
-;; - `ef-themes-load-random'
-;; - `ef-themes-preview-colors'
-;; - `ef-themes-preview-colors-current'
 
 (custom-set-faces
  '(treemacs-directory-face ((t (:foreground "unspecified-fg" :inherit default))))
  '(treemacs-file-face ((t (:foreground "unspecified-fg" :inherit default))))
- ;; 根目录图标颜色
  '(treemacs-nerd-icons-root-face ((t (:foreground "#179299"
                                                   :background "unspecified-bg"
                                                   :color "gtk_selection_bg_color"
                                                  
                                      :weight bold
                                      :height 1.4))))
-;; 文件夹图标颜色
+ 
 '(treemacs-nerd-icons-dir-face ((t (:foreground "#00B4D8"))))
 '(treemacs-root-face ((t (:foreground "#ff79c6"
                                       :background "unspecified-bg"
                                       :weight bold :height 1.4 :line-spacing 0.4))))
 
 
-;; 文件图标颜色
 '(treemacs-nerd-icons-file-face ((t (:foreground "#00B4D8"))))
 '(nerd-icons-green
   ((((background dark)) :foreground "#FA7343")  
