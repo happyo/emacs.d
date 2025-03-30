76693dd615a554dc6026a2cef2f27fc8ad916a03
happyo <happyojones@gmail.com>
2025-03-30 22:36:26 +0800

    refactor(emacs): improve vertico configuration and enable history persistence
    - Update vertico package configuration to specify branch and extensions directory
    - Enable vertico-multiform-mode in the config section
    - Remove redundant vertico-multiform package declaration
    - Enable savehist-mode for persisting minibuffer history

--- lisp/init-search.el
+++ lisp/init-search.el
@@ -32,11 +32,11 @@
 
 (use-package vertico
   :ensure t
-  :vc (:url "https://github.com/minad/vertico.git")
+  :vc (:url "https://github.com/minad/vertico.git" :branch "main" :lisp-dir "extensions/")
   :init
   (vertico-mode)
-;;  (vertico-multiform-mode)
   :config
+  (vertico-multiform-mode)
   (global-set-key (kbd "C-d") 'vertico-scroll-up)
   (global-set-key (kbd "C-u") 'vertico-scroll-down)
   (setq completion-styles '(fussy substring partial-completion initials))
@@ -55,14 +55,9 @@
   ;; Persist history over Emacs restarts. Vertico sorts by history position.
   )
 
-(use-package vertico-multiform
-  :ensure nil
-  :after vertico
-  )
-
-;; (use-package savehist
-;;   :init
-;;   (savehist-mode))
+(use-package savehist
+  :init
+  (savehist-mode))
 
 (use-package flx)
 
